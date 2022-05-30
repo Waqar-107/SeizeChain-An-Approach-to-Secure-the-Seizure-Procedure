147b46f2acfc9035e18f982fa196aea28c43c7c9
Waqar-107 <waqar.hassan866@gmail.com>
2022-05-31 00:28:32 +0600

    updated map

--- Prototype/SeizeChain-Frontend/src/app/views/Home/index.js
+++ Prototype/SeizeChain-Frontend/src/app/views/Home/index.js
@@ -37,27 +37,30 @@ const Home = () => {
 			<div className={classes.contentContainer}>
 				<div className={classes.gridroot}>
 					<Grid container spacing={5}>
-						<Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+						<Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
 							<div style={{ width: "100%" }}>
 								<BangladeshiMap />
 							</div>
 						</Grid>
-						<Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+
+						<Grid item xs={12} sm={12} md={4} lg={4} xl={4}>
+							<div style={{ width: "100%", display: "flex", justifyContent: "center" }}>
+								<Pie data={data.distributionOfSeizedData} />
+							</div>
+						</Grid>
+
+						<Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
 							<div style={{ width: "100%" }}>
 								<Bar options={data.currentYear.options} data={data.currentYear.data} />
 							</div>
 						</Grid>
-						<Grid item xs={12} sm={12} md={6} lg={6} xl={6}>
+						<Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
 							<div style={{ width: "100%" }}>
 								<Bar options={data.avgPerYear.options} data={data.avgPerYear.data} />
 							</div>
 						</Grid>
+
 						<Grid item xs={12} sm={12} md={6} lg={4} xl={4}>
-							<div style={{ width: "100%" }}>
-								<Pie data={data.distributionOfSeizedData} />
-							</div>
-						</Grid>
-						<Grid item xs={12} sm={12} md={6} lg={8} xl={8}>
 							<div style={{ width: "100%" }}>
 								<Line options={data.lineData.options} data={data.lineData.data} />
 							</div>
